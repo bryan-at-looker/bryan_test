fd7ba458f195b6f3aaea849a55f6653b5b22ca16
bryan-at-looker <bryan-at-looker@users.noreply.github.com>
2018-01-08 13:44:12 -0800

    update2

--- sankey_buggy2.js
+++ sankey_buggy2.js
@@ -1,6 +1,6 @@
 looker.plugins.visualizations.add({
-  id: "sankey_buggy2",
-  label: "Sankey Buggy2",
+  id: "sankey_buggy",
+  label: "Sankey Buggy",
   options: {
     color_range: {
       type: "array",
@@ -46,7 +46,7 @@ looker.plugins.visualizations.add({
       min_pivots: 0, max_pivots: 0,
       min_dimensions: 1, max_dimensions: undefined,
       min_measures: 1, max_measures: 1,
-    })) return; 
+    })) return;
     let d3 = d3v4;
 
     let width = element.clientWidth;
@@ -66,33 +66,33 @@ looker.plugins.visualizations.add({
     let color = d3.scaleOrdinal()
       .range(config.color_range);
 
-		let defs = svg.append('defs');
+    let defs = svg.append('defs');
 
-		let sankey = d3.sankey()
-				.nodeWidth(10)
-				.nodePadding(12)
-				.extent([[1, 1], [width - 1, height - 6]]);
+    let sankey = d3.sankey()
+        .nodeWidth(10)
+        .nodePadding(12)
+        .extent([[1, 1], [width - 1, height - 6]]);
 
-		let link = svg.append("g")
-				.attr("class", "links")
-				.attr("fill", "none")
-				.attr("stroke", "#fff")
-			.selectAll("path");
+    let link = svg.append("g")
+        .attr("class", "links")
+        .attr("fill", "none")
+        .attr("stroke", "#fff")
+      .selectAll("path");
 
-		let node = svg.append("g")
-				.attr("class", "nodes")
-				.attr("font-family", "sans-serif")
-				.attr("font-size", 10)
-			.selectAll("g");
+    let node = svg.append("g")
+        .attr("class", "nodes")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+      .selectAll("g");
 
-		let graph = {
-			nodes: [],
-			links: []
-		};
+    let graph = {
+      nodes: [],
+      links: []
+    };
 
-		let nodes = d3.set();
+    let nodes = d3.set();
 
-		data.forEach(function(d) {
+    data.forEach(function(d) {
       // variable number of dimensions
       let path = dimensions.map(function(dim) {return d[dim.name].value + ""});
       path.forEach(function(p,i) {
@@ -105,32 +105,32 @@ looker.plugins.visualizations.add({
                            "target": target,
                            "value": +d[measure.name].value});
       });
-		});
+    });
 
-		let nodesArray = nodes.values();
+    let nodesArray = nodes.values();
 
-		graph.links.forEach(function (d, i) {
-			d.source = nodesArray.indexOf(d.source);
-			d.target = nodesArray.indexOf(d.target);
-		});
+    graph.links.forEach(function (d, i) {
+      d.source = nodesArray.indexOf(d.source);
+      d.target = nodesArray.indexOf(d.target);
+    });
 
-		graph.nodes = nodes.values().map(function(d) {
-			return {
-				name: d.slice(0,-1)
-			};
-		});
+    graph.nodes = nodes.values().map(function(d) {
+      return {
+        name: d.slice(0,-1)
+      };
+    });
 
-		sankey(graph);
+    sankey(graph);
 
     var _s = this;
 
-		link = link
-			.data(graph.links)
-			.enter().append("path")
-				.attr("class", "link")
-				.attr("d", function(d) { return "M" + -10 + "," + -10 + d3.sankeyLinkHorizontal()(d); })
-				.style("opacity", 0.4)
-				.attr("stroke-width", function(d) { return Math.max(1, d.width); })
+    link = link
+      .data(graph.links)
+      .enter().append("path")
+        .attr("class", "link")
+        .attr("d", function(d) { return "M" + -10 + "," + -10 + d3.sankeyLinkHorizontal()(d); })
+        .style("opacity", 0.4)
+        .attr("stroke-width", function(d) { return Math.max(1, d.width); })
         .on("mouseenter", function(d) {
           svg.selectAll(".link")
             .style("opacity", 0.05)
@@ -160,37 +160,37 @@ looker.plugins.visualizations.add({
         .on('mouseover', _s.tipLinks.show)
         .on('mouseout', _s.tipLinks.hide);
 
-		// gradients https://bl.ocks.org/micahstubbs/bf90fda6717e243832edad6ed9f82814
-		link.style('stroke', function(d,i) {
+    // gradients https://bl.ocks.org/micahstubbs/bf90fda6717e243832edad6ed9f82814
+    link.style('stroke', function(d,i) {
 
-			// make unique gradient ids
-			let gradientID = "gradient" + i;
+      // make unique gradient ids
+      let gradientID = "gradient" + i;
 
-			let startColor = color(d.source.name.replace(/ .*/, ""));
-			let stopColor = color(d.target.name.replace(/ .*/, ""));
+      let startColor = color(d.source.name.replace(/ .*/, ""));
+      let stopColor = color(d.target.name.replace(/ .*/, ""));
 
-			let linearGradient = defs.append('linearGradient')
-					.attr('id', gradientID);
+      let linearGradient = defs.append('linearGradient')
+          .attr('id', gradientID);
 
-			linearGradient.selectAll('stop')
-				.data([
-						{offset: '10%', color: startColor },
-						{offset: '90%', color: stopColor }
-					])
-				.enter().append('stop')
-				.attr('offset', function(d) {
-					return d.offset;
-				})
-				.attr('stop-color', function(d) {
-					return d.color;
-				});
+      linearGradient.selectAll('stop')
+        .data([
+            {offset: '10%', color: startColor },
+            {offset: '90%', color: stopColor }
+          ])
+        .enter().append('stop')
+        .attr('offset', function(d) {
+          return d.offset;
+        })
+        .attr('stop-color', function(d) {
+          return d.color;
+        });
 
-			return "url(#" + gradientID + ")";
-		})
+      return "url(#" + gradientID + ")";
+    })
 
-		node = node
-			.data(graph.nodes)
-			.enter().append("g")
+    node = node
+      .data(graph.nodes)
+      .enter().append("g")
       .attr("class", "node")
       .on("mouseenter", function(d) {
         svg.selectAll(".link")
@@ -214,27 +214,27 @@ looker.plugins.visualizations.add({
       .on('mouseover', _s.tipNodes.show)
       .on('mouseout', _s.tipNodes.hide);
 
-		node.append("rect")
-				.attr("x", function(d) { return d.x0; })
-				.attr("y", function(d) { return d.y0; })
-				.attr("height", function(d) { return d.y1 - d.y0; })
-				.attr("width", function(d) { return d.x1 - d.x0; })
-				.attr("fill", function(d) { return color(d.name.replace(/ .*/, "")); })
-				.attr("stroke", "#555");
+    node.append("rect")
+        .attr("x", function(d) { return d.x0; })
+        .attr("y", function(d) { return d.y0; })
+        .attr("height", function(d) { return d.y1 - d.y0; })
+        .attr("width", function(d) { return d.x1 - d.x0; })
+        .attr("fill", function(d) { return color(d.name.replace(/ .*/, "")); })
+        .attr("stroke", "#555");
 
-		node.append("text")
-				.attr("x", function(d) { return d.x0 - 6; })
-				.attr("y", function(d) { return (d.y1 + d.y0) / 2; })
-				.attr("dy", "0.35em")
-				.style("font-weight", "bold")
-				.attr("text-anchor", "end")
-				.style("fill", "#222")
-				.text(function(d) { return d.name; })
-			.filter(function(d) { return d.x0 < width / 2; })
-				.attr("x", function(d) { return d.x1 + 6; })
-				.attr("text-anchor", "start");
+    node.append("text")
+        .attr("x", function(d) { return d.x0 - 6; })
+        .attr("y", function(d) { return (d.y1 + d.y0) / 2; })
+        .attr("dy", "0.35em")
+        .style("font-weight", "bold")
+        .attr("text-anchor", "end")
+        .style("fill", "#222")
+        .text(function(d) { return d.name; })
+      .filter(function(d) { return d.x0 < width / 2; })
+        .attr("x", function(d) { return d.x1 + 6; })
+        .attr("text-anchor", "start");
 
-		node.append("title")
-				.text(function(d) { return d.name + "\n" + d.value; });
+    node.append("title")
+        .text(function(d) { return d.name + "\n" + d.value; });
   }
 });
